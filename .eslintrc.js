e618233961a557ba3b79357a4c8666325024c390
Logan <logan@rallycry.gg>
2021-06-21 21:53:48 -0700

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   env: {
     node: true,
     commonjs: true,
     es6: true,
   },
-};
+  semi: [2, 'never'],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+}
