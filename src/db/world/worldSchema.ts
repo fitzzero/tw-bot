57c09a57b5bc237ac88ddbe5b58ccb9190f9f063
Logan <logan@rallycry.gg>
2021-11-01 22:32:27 -0700

    world unused

--- src/db/world/worldSchema.ts
+++ src/db/world/worldSchema.ts
@@ -1,10 +1,5 @@
-import moment from 'moment'
 import { Schema, model } from 'mongoose'
-import { isDev } from '../../config'
-import { getActiveWorld } from '../../loop'
-import { VoidFnProps } from '../../types/methods'
-import { UpdateWorld, World } from '../../types/world'
-import { logger } from '../../utility/logger'
+import { World } from '../../types/world'
 
 const schemaOptions = {
   toJSON: { virtuals: true },
