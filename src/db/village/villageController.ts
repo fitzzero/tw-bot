55d8e5d0a0438cdc6fe40a3fc8bfaf7c39f8bdb8
Logan <logan@rallycry.gg>
2021-11-01 23:05:20 -0700

    re-org

--- src/db/village/villageController.ts
+++ src/db/village/villageController.ts
@@ -1,19 +1,13 @@
-import { discordAlert } from '../../discord/alert'
-import { getActiveWorld } from '../../loop'
-import { Fn, PromiseFn } from '../../types/methods'
+import { PromiseFn } from '../../types/methods'
 import {
   BulkUpdateVillage,
   GetVillage,
   RemoveVillage,
-  RunVillageStats,
   UpdateVillage,
-  Village,
-  VillageStats,
 } from '../../types/village'
-import { Coordinate } from '../../types/world'
 import { logger } from '../../utility/logger'
-import { coordinatesInRange } from '../../utility/twUtility'
 import { VillageModel } from './villageSchema'
+import { stats } from './villageStats'
 
 export const cleanDeletedVillages: PromiseFn<BulkUpdateVillage, void> = async ({
   villageData,
@@ -56,101 +50,6 @@ export const removeVillage: PromiseFn<RemoveVillage, void> = async ({
     logger({ prefix: 'alert', message: `Database: ${err}` })
   }
   return
-}
-
-let testData = false
-let checkStats = true
-let start: Coordinate | undefined = undefined
-let radius: number | undefined = undefined
-let foundInRange = 0
-
-export const villagesInRange = (): number => foundInRange
-
-const statAlerts = (village: Village, newStats: VillageStats): void => {
-  if (!village.stats) return
-  if (checkStats && !start) {
-    const world = getActiveWorld()
-    if (world?.testData) {
-      start = { x: 500, y: 500 }
-      radius = 10
-      testData = true
-      logger({ prefix: 'success', message: `World: Using test stat values` })
-    } else if (!world?.radius || !world?.start) {
-      checkStats = false
-      logger({
-        prefix: 'alert',
-        message: `World: No start and radius values, skipping stats`,
-      })
-      return
-    } else {
-      start = world.start
-      radius = world.radius
-    }
-  }
-  if (!checkStats || !start || !radius) {
-    return
-  }
-  if (coordinatesInRange(start, radius, { x: village.x, y: village.y })) {
-    if (foundInRange === 0 && testData) {
-      newStats.lastPointDecrease = 0
-    }
-    if (foundInRange === 1 && testData) {
-      village.stats.stale = true
-    }
-    if (foundInRange === 2 && testData) {
-      newStats.stale = true
-      village.stats.stale = false
-    }
-    const oldStats = village.stats
-    foundInRange++
-    if (newStats.stale && !oldStats?.stale) {
-      discordAlert({
-        message: `Village: ${village._id} (${village.name}) has gone inactive`,
-      })
-    }
-    if (!newStats.stale && oldStats?.stale) {
-      discordAlert({
-        message: `Village: ${village._id} (${village.name}) no longer inactive`,
-      })
-    }
-    if (newStats.lastPointDecrease === 0 && oldStats.lastPointDecrease > 0) {
-      discordAlert({
-        message: `Village: ${village._id} (${village.name}) has just dropped points`,
-      })
-    }
-  }
-
-  return
-}
-
-const stats: Fn<RunVillageStats, Village> = ({ village, newData }) => {
-  let lastPointIncrease = village.stats?.lastPointIncrease || 0
-  let lastPointDecrease = village.stats?.lastPointDecrease || 0
-  let stale = false
-  if (village.points === newData.points) {
-    lastPointIncrease++
-    lastPointDecrease++
-  }
-  if (newData.points > village.points) {
-    lastPointIncrease = 0
-  }
-  if (newData.points < village.points) {
-    lastPointDecrease = 0
-  }
-  if (lastPointIncrease > 24) {
-    stale = true
-  }
-
-  const newStats: VillageStats = {
-    lastPointIncrease,
-    lastPointDecrease,
-    stale,
-  }
-
-  statAlerts(village, newStats)
-  village.stats = newStats
-
-  return village
 }
 
 export const updateOrCreateVillage: PromiseFn<UpdateVillage, void> = async ({
--- src/types/methods.d.ts
+++ src/types/methods.d.ts
@@ -1,3 +1,2 @@
 export type Fn<T1, T2> = (props: T1) => T2
 export type PromiseFn<T1, T2> = (props: T1) => Promise<T2>
-export type VoidFn = () => Promise<void>
