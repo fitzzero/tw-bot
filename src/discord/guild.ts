657cfc0dd8f3f293eb58bb1ba2e01f0b1c3d2b07
unknown <loganfarseer@gmail.com>
2022-07-30 12:04:07 -0700

    fix

--- src/discord/guild.ts
+++ src/discord/guild.ts
@@ -1,8 +1,8 @@
 import { Guild } from 'discord.js'
 import { discordConfig } from '../config'
-import { discord } from './connect'
+import { discordClient } from './connect'
 
 export const getActiveGuild = async (): Promise<Guild | undefined> => {
-  const guild = await discord.guilds.fetch(discordConfig().guild.id)
+  const guild = await discordClient.guilds.fetch(discordConfig().guild.id)
   return guild
 }
