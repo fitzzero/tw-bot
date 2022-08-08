1be9ce074dc2e7785ff6100312349fb5d687f296
unknown <loganfarseer@gmail.com>
2022-08-08 14:09:34 -0700

    bump ver

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { startDiscord } from './discord/connect'
 import { nowString } from './utility/time'
 
 export const BotInfo = {
-  version: '1.7',
+  version: '1.8',
   started: nowString(),
 }
 
