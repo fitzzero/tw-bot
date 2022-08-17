4eae7f0a4719be8983f440a72ffcbe37bc10a1f0
unknown <loganfarseer@gmail.com>
2022-08-17 11:26:16 -0700

    relative time

--- src/discord/alerts/conquerAlerts.ts
+++ src/discord/alerts/conquerAlerts.ts
@@ -36,7 +36,7 @@ export const conquerAlerts = async (conquer: ConquerData) => {
     }
     const payload = villageMessage({
       color: WRColors.gray,
-      description: `Has been taken from: ${conquered} <t:${conquer.unix}>`,
+      description: `Has been taken from: ${conquered} <t:${conquer.unix}:R>`,
       village,
     })
     await channels.sendMessage(WRChannels.news, payload)
